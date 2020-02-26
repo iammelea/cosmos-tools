8746400f68b4003f96a6426d44e4ecc28193c78c
iammelea <38863192+iammelea@users.noreply.github.com>
2020-02-26 02:09:58 +0100

    Update and rename status_restart_msg.sh to gaia_status_start_msg.sh

--- monitoro/gaia_status_start_msg.sh
+++ monitoro/gaia_status_start_msg.sh
@@ -1,4 +1,4 @@
-# bash script telegram bot 1,2,3 = status - if null start- then sync status msg  
+# bash script telegram bot 1,2,3 = status - if null start- then sync status msg. by melea
 while [ "true" ]
     do
         token="<YOUR_TELEGRAM_TOKEN>"
@@ -6,11 +6,11 @@ while [ "true" ]
         tg_api="https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}"
 
  
-        sync_status="true|false"
-        status=`gaiacli status | grep -E ${catching_up}`
+        catching_up="true|false"
+        status=`curl localhost:26657/status | grep -E ${catching_up}`
         if ! pgrep -x "gaiad" > /dev/null
         then
-            crash_msg="No response from the node Bro, let's restart this boy. . . 🚀"
+            crash_msg="Ups! node is gone, let's start this boy. . . 🚀"
             echo $crash_msg
             curl -s "${tg_api}" --data-urlencode "text=${crash_msg}"
             echo -e gaiad start &
@@ -18,8 +18,8 @@ while [ "true" ]
         else
             status_msg="Cosmos sync= ${status}!"
             echo "${status_msg}"
-            # Telegram notification
-            # If no need to notificate just comment line bellow
+            # Telegram msg
+            # If no wnat msg uncoment next line.
             curl -s "${tg_api}" --data-urlencode "text=${status_msg}"
         fi
         sleep 30

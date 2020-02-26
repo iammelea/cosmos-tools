93d46fde15545c8f3a79386a89a1c41949abdb7e
iammelea <38863192+iammelea@users.noreply.github.com>
2020-02-26 01:12:17 +0100

    Create status_restart_msg.sh

---
+++ monitoro/status_restart_msg.sh
@@ -0,0 +1,28 @@
+# bash script telegram bot 1,2,3 = status - if null start- then sync status msg  
+while [ "true" ]
+    do
+        token="<YOUR_TELEGRAM_TOKEN>"
+        chat_id="<YOUR_CHAT_ID>"
+        tg_api="https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}"
+
+ 
+        sync_status="true|false"
+        status=`gaiacli status | grep -E ${"catching_up}`
+        if ! pgrep -x "gaiad" > /dev/null
+        then
+            crash_msg="No response from the node Bro, let's restart this boy. . . 🚀"
+            echo $crash_msg
+            curl -s "${tg_api}" --data-urlencode "text=${crash_msg}"
+            echo -e gaiad start &
+            sleep 180
+        else
+            status_msg="Cosmos sync= ${status}!"
+            echo "${status_msg}"
+            # Telegram notification
+            # If no need to notificate just comment line bellow
+            curl -s "${tg_api}" --data-urlencode "text=${status_msg}"
+        fi
+        sleep 30
+            
+done
+

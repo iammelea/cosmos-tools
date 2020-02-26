9f3a3b19c0d8acc8f9278462689fba3575fa11b1
iammelea <38863192+iammelea@users.noreply.github.com>
2020-02-26 19:39:18 +0100

    Update and rename regen_status_start_msg.sh to xrnd_moni.sh

--- monitoro/xrnd_moni.sh
+++ monitoro/xrnd_moni.sh
@@ -1,27 +1,29 @@
-# bash script telegram bot 1,2,3 = Cosmos SDK nodes→ status - if null→ start- then sync status msg. by melea
+# bash script telegram bot 1,2,3 = status - if null start- then sync status msg  
 while [ "true" ]
     do
-        token="<WRITE-YOUR_TELEGRAM_TOKEN>"
-        chat_id="<WRITE-YOUR_CHAT_ID>"
+        token="588327724:AAHVA3s2BscgMXXJ96R6BtpwU2Hh8b_p8SY"
+        chat_id="364639716"
         tg_api="https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}"
 
- 
+
+
         catching_up="true|false"
         status=`curl localhost:26657/status | grep -E ${catching_up}`
+        peers=`curl localhost:26657/net_info | grep -E n_peers`
         if ! pgrep -x "xrnd" > /dev/null
         then
-            crash_msg="UPS! node is not working, let's start this boy. . . 🚀 "
+            crash_msg="☢️No response‼️ from the node☢️, let's restart this boy. . . 🚀 "
             echo $crash_msg
             curl -s "${tg_api}" --data-urlencode "text=${crash_msg}"
             echo -e screen -S regen && xrnd start &
             sleep 1800
         else
-            status_msg="REGEN sync: ${status}!"
+            status_msg="Regen V: 💻 ⛏${peers} ⚛️  ${status} ⚙️"
             echo "${status_msg}"
             # Telegram notification
-            # If no want msg uncoment next line.
+            # If no need to notificate just comment line bellow
             curl -s "${tg_api}" --data-urlencode "text=${status_msg}"
         fi
         sleep 900
 
-done        
+done

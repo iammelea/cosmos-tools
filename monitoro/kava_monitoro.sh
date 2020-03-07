ee9b1859c0e66c3a0412cb992c32d8b429953ebb
iammelea <38863192+iammelea@users.noreply.github.com>
2020-03-07 07:19:24 +0100

    Create kava_monitoro.sh

---
+++ monitoro/kava_monitoro.sh
@@ -0,0 +1,23 @@
+# bash script Cosmos Monitor with telegram msg.→ what do? =  send telegram msg with status + peers then check if the node is not running restart + msg telegram 
+#EDIT where: 26657 for your RPC port if it is a different one. + <YOUR_TELEGRAM_TOKEN> + <YOUR_CHAT_ID> + <WRITE-NODE-NAME-HERE> + <sudo systemctl restart ???>
+while [ "true" ]
+    do
+        token="<YOUR_TELEGRAM_TOKEN>"
+        chat_id="<YOUR_CHAT_ID>"
+        tg_api="https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}"
+        status=`curl http://localhost:26657/status | grep -E catching_up`
+        peers=`curl  http://localhost:26657/net_info | grep -E n_peers`
+        status_msg="💱 KaVa V:  🔖    ${peers} & ${status} ⛏⚙️"
+            echo "${status_msg}"
+            curl -s "${tg_api}" --data-urlencode "text=${status_msg}" &
+        if ! curl http://localhost:26657/status > /dev/null
+        then
+            crash_msg="☢️Hum, 💱 KaVa V is Not running☢️. Restarting now‼️ . . 🚀"
+            echo $crash_msg
+            curl -s "${tg_api}" --data-urlencode "text=${crash_msg}"
+            rst=`sudo systemctl restart kava`
+            echo $rst  &
+        fi
+            sleep 1800
+
+    done

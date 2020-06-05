3d3bb72c4eed9f6bc75f459a640d0cbdc607e811
iammelea <38863192+iammelea@users.noreply.github.com>
2020-06-05 08:51:45 +0200

    Create go_install.sh

---
+++ IBC-GOZ/automotion/scripts/go_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#setup GOPATH
+            echo "setup GOPATH 1,2,3" 
+            echo 'GOPATH="$HOME/go"' >> $HOME/.profile
+            echo 'PATH="$GOPATH/bin:$PATH"' >> $HOME/.profile
+            echo 'PATH="$PATH:/usr/local/go/bin"' >> $HOME/.profile
+            source $HOME/.profile
+
+       command -v go &> /dev/null
+        if [ $? -ne 0 ]
+
+            then
+            echo "GO aka Goland is not installed, preparing for install GO and setup the GOPATH"
+            sudo apt-get update
+            echo "Installing go now, visit https://golang.org/ for more info"
+            wget https://dl.google.com/go/go1.14.linux-amd64.tar.gz 
+            sudo tar -C /usr/local -xzf go1.14.linux-amd64.tar.gz
+            
+            go version
+        else
+            echo    "go is already installed on your system, skipping goland installation"
+        fi

3a63b5217471462119dbc96e737b4915d16b1ab4
iammelea <38863192+iammelea@users.noreply.github.com>
2020-06-05 10:32:32 +0200

    Create gaia_install.sh

---
+++ IBC-GOZ/automotion/scripts/gaia_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#here we go
+command -v go >/dev/null 2>&1 || { echo >&2 "Here require Goland aKa GO but it's not installed or GOPATH is not oke. check ~/cosmos-tools/ibc/scripts and Install goland, Now  Aborting."; exit 1; }
+
+#install gaia & relayer
+
+git clone https://github.com/cosmos/gaia && cd gaia && git checkout goz-phase-2 && make install && gaiad version --long
+cd
+git clone https://github.com/iqlusioninc/relayer && cd relayer && make install && rly version
+cd

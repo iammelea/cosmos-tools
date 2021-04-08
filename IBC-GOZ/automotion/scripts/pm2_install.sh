226e039a4a52d5ecbff948a7599cf668ec80af1e
iammelea <38863192+iammelea@users.noreply.github.com>
2021-04-08 21:17:51 +0200

    Update pm2_install.sh
    update nvm 038

--- IBC-GOZ/automotion/scripts/pm2_install.sh
+++ IBC-GOZ/automotion/scripts/pm2_install.sh
@@ -6,7 +6,7 @@
             echo "PM2 is not installed, preparing for install or update: nvm, nodes, npm, pm2"
             sudo apt-get update && sudo apt install build-essential jq -y
             echo "Installing nvm nodes npm pm2"
-            curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+            curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
             export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
             [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
                 source $HOME/.profile
